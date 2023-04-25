f8fca845474dc22cdc57ded9845200fa1838863c
Tayyib <ic20b101@technikum-wien.at>
2023-04-25 19:12:22 +0200

    add Controller add Service add Test

--- src/test/java/fhwien/BMI/BmiApplicationTests.java
+++ src/test/java/fhwien/BMI/BmiApplicationTests.java
@@ -1,13 +1,25 @@
 package fhwien.BMI;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class BmiApplicationTests {
 
+
+	@Autowired
+	private BMIService bmiService;
+
 	@Test
-	void contextLoads() {
+	public void testCalculateBMI() {
+		double weight = 80;
+		double height = 1.85;
+		double expected = 23.37;
+		double result = bmiService.calculateBMI(weight, height);
+		assertEquals(expected, result, 0.01);
 	}
 
 }
